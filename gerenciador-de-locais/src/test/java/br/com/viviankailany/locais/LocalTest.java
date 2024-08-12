e3cb54ba5a20313bc4d2c24f8b70e4bcc7bf6a01
Vivian Kailany <you@example.com>
2024-08-12 20:29:31 -0300

    Testes unitários.

--- gerenciador-de-locais/src/test/java/br/com/viviankailany/locais/LocalTest.java
+++ gerenciador-de-locais/src/test/java/br/com/viviankailany/locais/LocalTest.java
@@ -7,6 +7,7 @@ import jakarta.validation.ValidatorFactory;
 import jakarta.validation.ConstraintViolation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -18,17 +19,21 @@ public class LocalTest {
 
     @BeforeEach
     public void setup() {
+        // Configuração do validador a partir da fábrica padrão
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         validator = factory.getValidator();
     }
 
     @Test
     public void whenNomeIsBlank_thenValidationFails() {
+        // Criação de um objeto Local com nome em branco
         Local local = new Local();
         local.setNome("   "); // String com apenas espaços em branco
 
+        // Validação do objeto
         Set<ConstraintViolation<Local>> violations = validator.validate(local);
 
+        // Verificação das violações de validação
         assertFalse(violations.isEmpty());
         assertTrue(violations.stream()
                 .anyMatch(v -> v.getMessage().equals("O nome do local é obrigatório.")));
@@ -36,50 +41,94 @@ public class LocalTest {
 
     @Test
     public void whenNomeIsNull_thenValidationFails() {
+        // Criação de um objeto Local com nome nulo
         Local local = new Local();
-        local.setNome(null); // Nome é nulo
+        local.setNome(null);
 
+        // Validação do objeto
         Set<ConstraintViolation<Local>> violations = validator.validate(local);
 
+        // Verificação das violações de validação
         assertFalse(violations.isEmpty());
         assertTrue(violations.stream()
                 .anyMatch(v -> v.getMessage().equals("O nome do local é obrigatório.")));
     }
 
     @Test
-    public void whenNomeIsTooLong_thenValidationFails() throws Exception {
-        // Cria um objeto Local com descrição muito longa
+    public void whenNomeIsTooLong_thenValidationFails() {
+        // Criação de um objeto Local com nome muito longo
         String nome = "a".repeat(101);
         Local local = new Local();
         local.setNome(nome);
+
+        // Validação do objeto
         Set<ConstraintViolation<Local>> violations = validator.validate(local);
 
+        // Verificação das violações de validação
         assertFalse(violations.isEmpty());
         assertTrue(violations.stream()
-                .anyMatch(v -> v.getMessage().equals("A descrição do local deve ter no máximo 100 caracteres.")));
+                .anyMatch(v -> v.getMessage().equals("O nome deve ter no máximo 100 caracteres.")));
     }
 
     @Test
-    public void whenDescricaoIsTooLong_thenValidationFails() throws Exception {
-        // Cria um objeto Local com descrição muito longa
-        String descricao = "a".repeat(256);
+    public void whenBairroIsTooLong_thenValidationFails() {
+        // Criação de um objeto Local com bairro muito longo
+        String bairro = "a".repeat(256);
         Local local = new Local();
-        local.setBairro(descricao);
+        local.setBairro(bairro);
+
+        // Validação do objeto
         Set<ConstraintViolation<Local>> violations = validator.validate(local);
 
-            assertFalse(violations.isEmpty());
-            assertTrue(violations.stream()
-                    .anyMatch(v -> v.getMessage().equals("A descrição do local deve ter no máximo 255 caracteres.")));
+        // Verificação das violações de validação
+        assertFalse(violations.isEmpty());
+        assertTrue(violations.stream()
+                .anyMatch(v -> v.getMessage().equals("O bairro deve ter no máximo 100 caracteres.")));
     }
 
-
     @Test
-    public void whenNomeIsValid_thenValidationPasses() {
+    public void whenCidadeIsBlank_thenValidationFails() {
+        // Criação de um objeto Local com cidade em branco
         Local local = new Local();
-        local.setNome("Local Válido"); // Nome válido
+        local.setCidade("   ");
 
+        // Validação do objeto
         Set<ConstraintViolation<Local>> violations = validator.validate(local);
 
+        // Verificação das violações de validação
+        assertFalse(violations.isEmpty());
+        assertTrue(violations.stream()
+                .anyMatch(v -> v.getMessage().equals("O nome da cidade é obrigatório.")));
+    }
+
+    @Test
+    public void whenEstadoIsBlank_thenValidationFails() {
+        // Criação de um objeto Local com estado em branco
+        Local local = new Local();
+        local.setEstado("   ");
+
+        // Validação do objeto
+        Set<ConstraintViolation<Local>> violations = validator.validate(local);
+
+        // Verificação das violações de validação
+        assertFalse(violations.isEmpty());
+        assertTrue(violations.stream()
+                .anyMatch(v -> v.getMessage().equals("O nome do estado é obrigatório.")));
+    }
+
+    @Test
+    public void whenAllFieldsAreValid_thenValidationPasses() {
+        // Criação de um objeto Local com todos os campos válidos
+        Local local = new Local();
+        local.setNome("Praça da Fonte");
+        local.setBairro("Centro");
+        local.setCidade("Pacatuba");
+        local.setEstado("CE");
+
+        // Validação do objeto
+        Set<ConstraintViolation<Local>> violations = validator.validate(local);
+
+        // Verificação das violações de validação
         assertTrue(violations.isEmpty());
     }
 }
